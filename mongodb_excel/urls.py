4a731e347ab4b443b842d58cc7695f36cec34a59
Abrar hussain dar <63048954+Abrarhussaindar@users.noreply.github.com>
2024-04-18 12:29:02 +0530

    add excel for both databases

--- mongodb_excel/urls.py
+++ mongodb_excel/urls.py
@@ -1,7 +1,8 @@
 # mongodb_excel/urls.py
 from django.urls import path
-from .views import download_excel
+from . import views
 
 urlpatterns = [
-    path('download_excel/', download_excel, name='download_excel'),
+    path('download_skillgap_excel/', views.download_skillgap_excel, name='download_skillgap_excel'),
+    path('download_youthaspiration_excel/', views.download_youthaspiration_excel, name='download_youthaspiration_excel'),
 ]
--- mongodb_excel/views.py
+++ mongodb_excel/views.py
@@ -5,18 +5,17 @@ from pymongo import MongoClient
 from io import BytesIO
 import pandas as pd
 
-def download_excel(request):
-    # Connect to MongoDB
+def download_skillgap_excel(request):
     mongo_uri = "mongodb+srv://abrardar988651:Abrardar123@freeserver.wc1ytkf.mongodb.net/?retryWrites=true&w=majority&appName=freeServer"
 
     # Connect to MongoDB Atlas
     client = MongoClient(mongo_uri)
-    db = client['test']
+    db = client['SkillGap']
     collection = db['users']
 
     # Query the data
     cursor = collection.find({})  # You can add filters if needed
-
+    print("cursor: ", cursor)
     # Convert cursor to list of dictionaries
     data = list(cursor)
 
@@ -33,6 +32,38 @@ def download_excel(request):
         excel_output.getvalue(),
         content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
     )
-    response['Content-Disposition'] = 'attachment; filename=mongodb_data.xlsx'
+    response['Content-Disposition'] = 'attachment; filename=SkillGap_data.xlsx'
+
+    return response
+
+def download_youthaspiration_excel(request):
+    # Connect to MongoDB
+    mongo_uri = "mongodb+srv://abrardar988651:Abrardar123@freeserver.wc1ytkf.mongodb.net/?retryWrites=true&w=majority&appName=freeServer"
+
+    # Connect to MongoDB Atlas
+    client = MongoClient(mongo_uri)
+    db = client['YouthAspiration']
+    collection = db['users']
+
+    # Query the data
+    cursor = collection.find({})  # You can add filters if needed
+    print("cursor: ", cursor)
+    # Convert cursor to list of dictionaries
+    data = list(cursor)
+
+    # Create DataFrame from the list of dictionaries
+    df = pd.DataFrame(data)
+
+    # Create Excel file in memory
+    excel_output = BytesIO()
+    df.to_excel(excel_output, index=False)
+    excel_output.seek(0)
+
+    # Set up HTTP response
+    response = HttpResponse(
+        excel_output.getvalue(),
+        content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    )
+    response['Content-Disposition'] = 'attachment; filename=YouthAspiration_data.xlsx'
 
     return response
